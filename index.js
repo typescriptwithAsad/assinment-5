e470e0130b027f59135b641ca108341b52e96538
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-18 17:56:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+let famous_Person = "IMRAN KHAN";
+let message = `${famous_Person} said, "Tabdili ayee nahi Tabdili a gaye hai"`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_Person :string = "IMRAN KHAN";
+let message : string = `${famous_Person} said, "Tabdili ayee nahi Tabdili a gaye hai"`;
+console.log(message);
